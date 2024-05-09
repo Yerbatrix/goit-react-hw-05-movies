94cac811f0703cf044a0cb66d59180769318c1e5
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-05-09 16:20:51 +0200

    trending-movies-in-home

---
+++ src/config.js
@@ -0,0 +1,3 @@
+const API_KEY = '653bcff9e26cb831d0fcc6da9ef61d32';
+
+export default API_KEY;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import API_KEY from '../config';
+
 export const Home = () => {
-  return <div>Here will be Trending Movies</div>;
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+        );
+        setTrendingMovies(response.data.results);
+      } catch (error) {
+        console.log('Error fetching trending movies', error);
+      }
+    };
+
+    fetchTrendingMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending Movies Today</h1>
+      <ul>
+        {trendingMovies.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
